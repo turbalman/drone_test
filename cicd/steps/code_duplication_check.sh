c28e1d92fb8f5bbdd8263c5a2d7f4bb4c07a454d
yuanfeng.zhu <yuanfeng.zhu@mthreads.com>
2021-02-26 14:51:14 -0800

    Update code_duplication_check.sh

--- cicd/steps/code_duplication_check.sh
+++ cicd/steps/code_duplication_check.sh
@@ -7,11 +7,11 @@ echo $DRONE_BUILD_KEY
 
 if [[ $DRONE_SOURCE_BRANCH == "main" ]] && [[ $DRONE_TARGET_BRANCH == "main" ]];then
   if [[ $DRONE_BUILD_KEY == "pipeline_1" ]];then
-      echo "pipeline_1->code_style_check.sh started!"
-      echo "pipeline_1->code_style_check.sh finished!"
+      echo "pipeline_1->code_duplication_check.sh started!"
+      echo "pipeline_1->code_duplication_check.sh finished!"
   elif [[ $DRONE_BUILD_KEY == "pipeline_2" ]];then
-      echo "pipeline_2->code_style_check.sh started!"
-      echo "pipeline_2->code_style_check.sh finished!"
+      echo "pipeline_2->code_duplication_check.sh started!"
+      echo "pipeline_2->code_duplication_check.sh finished!"
   else
       echo "Pipeline $DRONE_BUILD_KEY did nothing in this step"
   fi
